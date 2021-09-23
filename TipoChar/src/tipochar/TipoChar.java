ec9bc2f09f1e1788c30da8811df3e21aa308e488
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-09-23 10:26:41 +0200

    VariableChar

---
+++ TipoChar/src/tipochar/TipoChar.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipochar;
+
+/**
+ *
+ * @author Lukelar
+ */
+public class TipoChar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char caracter = 'b';
+        System.out.println("caracter = " + caracter);
+        char caracter2= '\u0021';
+        System.out.println("caracter2 = " + caracter2);
+        char caracter3=33;
+        System.out.println("caracter3 = " + caracter3);
+        char caracter4 = '!';
+        System.out.println("caracter4 = " + caracter4);
+    }
+    
+}
